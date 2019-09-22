c13542b2004544e44bf1bc181c33c23e3bca4e93
zhouxin <840137893@qq.com>
2019-09-22 23:28:40 +0800

    导航菜单完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,22 +1,32 @@
 Page({
     data: {
-        swiperList: [
-
-        ]
+        swiperList: [],
+        cataList:[]
     },
     onLoad() {
         this.getSwiperDate()
+        this.getCateData()
     },
     getSwiperDate() {
         wx.request({
             url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
-            success: (result) => {
-                console.log(result);
+            success: (res) => {
+                // console.log(res);
                 this.setData({
-                    swiperList: result.data.message
+                    swiperList: res.data.message
                 })
             },
         });
     },
-
+    getCateData(){
+        wx.request({
+            url:'https://api.zbztb.cn/api/public/v1/home/catitems',
+            success:(res)=>{
+                console.log(res)
+                this.setData({
+                    cataList:res.data.message
+                })
+            }
+        })
+    }
 })
