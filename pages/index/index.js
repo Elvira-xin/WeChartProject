b7cc34584adb0ed8bb7e3d942c89ec36992d4709
zhouxin <840137893@qq.com>
2019-09-22 23:02:39 +0800

    首页轮播图完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,3 +1,22 @@
 Page({
-  
+    data: {
+        swiperList: [
+
+        ]
+    },
+    onLoad() {
+        this.getSwiperDate()
+    },
+    getSwiperDate() {
+        wx.request({
+            url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
+            success: (result) => {
+                console.log(result);
+                this.setData({
+                    swiperList: result.data.message
+                })
+            },
+        });
+    },
+
 })
