78814933181ef37d63efa64b4936ac2c452e9e19
zhouxin <840137893@qq.com>
2019-09-23 01:00:44 +0800

    首页楼层部分完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,12 +1,18 @@
 Page({
     data: {
+        //轮播图数组
         swiperList: [],
-        cataList:[]
+        //导航菜单数组
+        cataList:[],
+        //楼层数组
+        floorList:[]
     },
     onLoad() {
         this.getSwiperDate()
         this.getCateData()
+        this.getFloorData()
     },
+    //获取轮播图数据
     getSwiperDate() {
         wx.request({
             url: 'https://api.zbztb.cn/api/public/v1/home/swiperdata',
@@ -18,13 +24,26 @@ Page({
             },
         });
     },
+    //获取导航菜单数据
     getCateData(){
         wx.request({
             url:'https://api.zbztb.cn/api/public/v1/home/catitems',
             success:(res)=>{
-                console.log(res)
+                // console.log(res)
                 this.setData({
                     cataList:res.data.message
+                })
+            }
+        })
+    },
+    //获取楼层数据
+    getFloorData(){
+        wx.request({
+            url:'https://api.zbztb.cn/api/public/v1/home/floordata',
+            success:(res)=>{
+                console.log(res);
+                this.setData({
+                    floorList:res.data.message
                 })
             }
         })
