b80eb86ec74fb098e3084e58022e2626fa770b5f
zhouxin <840137893@qq.com>
2019-09-24 00:51:40 +0800

    分类缓存效果实现

--- pages/category/index.js
+++ pages/category/index.js
@@ -9,19 +9,48 @@ Page({
   data: {
     menuList:[],
     goodsData:[],
-    currentIndex:0
-
+    currentIndex:0,
+    scrollTop: 0
   },
   Cates:[],
   onLoad(){
-    this.getCates()
+    this.loadData()
+  },
+  // 获取缓存中的数据获取接口中的数据
+  loadData(){
+    //  1 获取本地存储中的数据  值空 字符串
+    const localCate=wx.getStorageSync('cates');
+    // 2 判断该数据是否存在
+    if(localCate){
+      // 存在 判断该数据是否过期
+      if(Date.now() - localCate.time>1000*10){
+        // 过期了
+        this.getCates()
+      }else{
+        //未过期
+        this.Cates=localCate.data;
+        const menuList=this.Cates.map(v=> v.cat_name)
+        const goodsData=this.Cates[0].children
+        this.setData({
+          menuList,
+          goodsData
+        })
+      }
+    }else{
+      //数据不存在
+      this.getCates()
+    }
   },
   getCates(){
     request({
       url:'/categories'
     }).then(res=>{
       console.log(res)
-      this.Cates=res.data.message
+      this.Cates=res.data.message;
+      wx.setStorageSync('cates', {
+        data:this.Cates,
+        time:Date.now()
+      });
       const menuList=this.Cates.map(v=> v.cat_name)
       const goodsData=this.Cates[0].children
       this.setData({
@@ -39,7 +68,8 @@ Page({
     const goodsData=this.Cates[index].children
     this.setData({
       currentIndex:index,
-      goodsData
+      goodsData,
+      scrollTop: 0
     })
   }
 })
