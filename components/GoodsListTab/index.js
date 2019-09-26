cf598c7432be0c8dcee06b212b59e43a63ea297c
zhouxin <840137893@qq.com>
2019-09-27 00:01:29 +0800

    商品列表页渲染完成

---
+++ components/GoodsListTab/index.js
@@ -0,0 +1,34 @@
+// components/GoodsListTab/index.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    tabList:{
+      type:Array,
+      value:[]
+    },
+    currentIndex:{
+      type:Number,
+      value:0
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    handleTap(e){
+      // console.log(e);
+      const {index}=e.target.dataset;
+      this.triggerEvent('tabChange',{index})
+    },
+  }
+})
