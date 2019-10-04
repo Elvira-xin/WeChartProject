202d3088649f725e306a1f58e078a930f408b9d0
zhouxin <840137893@qq.com>
2019-10-04 13:22:18 +0800

    实现获取收获地址功能

--- pages/cart/index.js
+++ pages/cart/index.js
@@ -3,6 +3,35 @@ Page({
   data: {
 
   },
-
-  
+  getAddress() {
+    //获取用户当前设置
+    wx.getSetting({
+      success:(res)=> {
+        console.log(res)
+        const auth = res.authSetting["scope.address"]
+        //判断用户是否点击了取消
+        if (auth == false) {
+          //诱导用户打开授权
+          wx.openSetting({
+            success: (res2) => {
+              // console.log(res2);
+              //获取用户收获地址
+              wx.chooseAddress({
+                success:(res3)=> {
+                  console.log(res3);
+                }
+              })
+            }
+          })
+        }else{
+          //未点击取消 获取用户收货地址
+          wx.chooseAddress({
+            success:(res3)=> {
+              console.log(res3);
+            }
+          })
+        }
+      }
+    })
+  }
 })
